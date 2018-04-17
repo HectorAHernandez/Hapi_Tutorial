174d3c06ec25addb7b95f1a315f171cb36365e04
HectorAHernandez <38024843+HectorAHernandez@users.noreply.github.com>
2018-04-17 17:16:10 -0400

    Add files via upload
    adding the node-fetch Example.

---
+++ node-fetchRequestExample.js
@@ -0,0 +1,28 @@
+const fetch = require("node-fetch");
+const url =
+"http://localhost:3000/Sugar/productdetail------";
+//"http://localhost:3000/ORD-2541/product";
+//"http://localhost:3000/Julio/order";
+//"http://localhost:3000/customer";
+//"https://api.github.com/users/mitocode21"; 
+
+fetch(url)
+  .then((response) => {
+    return response.text();   //return only the text part of the respose if response is in text.
+    //return response.json(); //return only the json part of the respose if response is in json.
+    //console.log(response);  // this alone, log the whole response with header, status, body,etc
+  })
+  .then((responseConverted) => {
+      console.log(responseConverted);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
+/*
+fetch(url)
+  .then((res) => res.text()) //return only the text part of the respose if response is in text.
+  //.then((res) => res.json()) //return only the json part of the respose if response is in json.
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error))
+*/
